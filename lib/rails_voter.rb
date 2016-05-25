745497403a32ed8b5ac3faaabeddbfcf856b6c35
Fabian Kędzierski <grafska@gmail.com>
2016-05-25 22:34:29 +0200

    Initialize project

---
+++ lib/rails_voter.rb
@@ -0,0 +1,9 @@
+require "rails_voter/version"
+
+module RailsVoter
+  class TestInfo
+    def hello
+      "Hello SunnyRails!"
+    end
+  end
+end
---
+++ lib/rails_voter/version.rb
@@ -0,0 +1,3 @@
+module RailsVoter
+  VERSION = "0.1.0"
+end
